8ae82b5c467106d472f59de3e558ab803fa8d2d0
Shaikh Abdul Amaan <thisisamaanab@gmail.com>
2025-09-18 11:46:17 +0530

    initial commit

---
+++ lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:amazon_clone/constants/global_variable.dart';
+import 'package:amazon_clone/router.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Amazon clone',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: const ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      onGenerateRoute: (settings) => generateRoute(settings),
+      home: Scaffold(
+        appBar: AppBar(title: Text('Hello')),
+        body: const Center(child: Text('Flutter demo home page')),
+      ),
+    );
+  }
+}
